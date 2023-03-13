52d2f06233ac2958ead0001de169e3a7e715cc75
Emmanuel <adjeimanue@gmail.com>
2023-03-13 17:14:30 +0000

    kotlin spring boot setup

---
+++ src/main/kotlin/com/code8/bankingsystem/BankingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.code8.bankingsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/code8/bankingsystem/BankingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.code8.bankingsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
